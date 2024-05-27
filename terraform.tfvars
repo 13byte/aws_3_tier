a36c17bd1a79a41f7f15f65d3071126c937f6e61
jeosong <13bitadc@naver.com>
2024-05-27 13:37:46 +0900

    add vpc and subnet and rt and sg configuration

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,6 @@
-aws_region = "ap-northeast-2"
+aws_region   = "ap-northeast-2"
+vpc_name     = "jeosong-apne2"
+cidr_numeral = "10"
+
+availability_zones = ["ap-northeast-2a", "ap-northeast-2c"]
+short_azs          = ["2a", "2c"]
--- variables.tf
+++ variables.tf
@@ -1,3 +1,45 @@
 variable "aws_region" {
   default = "ap-northeast-2"
 }
+
+variable "vpc_name" {
+  description = "The VPC name"
+}
+
+variable "cidr_numeral" {
+  description = "The VPC CIDR numeral (10.x.0.0/16)"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "A comma-delimited list of availability zones for the VPC."
+}
+
+variable "short_azs" {
+  type        = list(string)
+  description = "A comma-delimited list of availability zones for the VPC."
+}
+
+variable "cidr_numeral_public" {
+  default = {
+    "0" = "0"
+    "1" = "16"
+    "2" = "32"
+  }
+}
+
+variable "cidr_numeral_private_was" {
+  default = {
+    "0" = "80"
+    "1" = "96"
+    "2" = "112"
+  }
+}
+
+variable "cidr_numeral_private_db" {
+  default = {
+    "0" = "160"
+    "1" = "176"
+    "2" = "192"
+  }
+}
