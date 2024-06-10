af7ed7ea9b241fa9f3231f1c839569beff61e70e
jeosong <13bitadc@naver.com>
2024-06-10 16:31:19 +0900

    add acm for alb

--- acm.tf
+++ acm.tf
@@ -16,3 +16,20 @@ resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
 }
+
+resource "aws_acm_certificate" "alb" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  subject_alternative_names = ["*.${var.domain_name}"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "alb" {
+  certificate_arn         = aws_acm_certificate.alb.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+}
+
