eb68849fbf89371644824df31fc19abf516aeb9b
jeosong <13bitadc@naver.com>
2024-06-10 16:32:45 +0900

    change https only and custom header, default action

--- lb.tf
+++ lb.tf
@@ -21,13 +21,37 @@ resource "aws_lb_target_group" "was_tg" {
   }
 }
 
-resource "aws_lb_listener" "back_end" {
+resource "aws_lb_listener" "backend" {
   load_balancer_arn = aws_lb.origin_lb.arn
-  port              = "80"
-  protocol          = "HTTP"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = aws_acm_certificate.alb.arn
 
   default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Forbidden"
+      status_code  = "403"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "host_based_weighted_routing" {
+  listener_arn = aws_lb_listener.backend.arn
+  priority     = 1
+
+  action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.was_tg.arn
+  }
+
+  condition {
+    http_header {
+      http_header_name = "X-Custom-Header"
+      values           = ["}Y$ILp#~sY{qAA1"]
+    }
   }
 }
